9c44d90148e2c748cc51b5b50f4c953bc4588224
Christian Faxholm <chrfaxh@dfds.com>
2023-07-26 13:32:24 +0200

    Added avro specific version of Dafda interfaces.

---
+++ src/Dafda.Avro/Consuming/Interfaces/IConsumerScopeFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dafda.Avro.Consuming.Interfaces
+{
+    public interface IConsumerScopeFactory<TResult>
+    {
+        /// <summary>
+        /// Creates consumer scope with result type defined in TResult
+        /// </summary>
+        IConsumerScope<TResult> CreateConsumerScope();
+    }
+}
